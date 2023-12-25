3462737bd3a2285f9f1e095c6fe8688ad59f3b11
Magitian <magitian@duck.com>
2023-12-25 16:08:45 +0530

    Yet to get GNOME wallpaper, resolution working

---
+++ airootfs/etc/profile.d/environment.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# If it's a Wayland session, set some environment variables.
+
+if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+	export QT_QPA_PLATFORM=wayland
+	export MOZ_ENABLE_WAYLAND=1
+fi
+
+# Set some other environment variables
+
+export QT_STYLE_OVERRIDE=kvantum
+
