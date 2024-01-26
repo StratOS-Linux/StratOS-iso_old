3f1383dbdca3b87df688f8c3ff43d981d0b9c5c7
zstg <o0vckutt@duck.com>
2024-01-26 16:39:33 +0530

    Lots of minor changes

--- profiledef.sh
+++ profiledef.sh
@@ -3,8 +3,8 @@
 
 iso_name="StratOS"
 iso_label="StratOS_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="LUGVITC <https://lugvitc.org>"
-iso_application="Live ISO for LUGVITC's Linux distro"
+iso_publisher="StratOS Team <https://github.com/StratOS-Linux/StratOS>"
+iso_application="StratOS Live ISO"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="stratos"
 buildmodes=('iso')
@@ -18,16 +18,16 @@ airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' 
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/etc/gshadow"]="0:0:400"
+  ["/opt/stratos-maneki-neko/main.py"]="0:0:777"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
   ["/root/.gnupg"]="0:0:700"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
-  ["/usr/local/bin/Installation_guide"]="0:0:755"
+  ["/usr/local/bin/install-bedrock-x86_64"]="0:0:777"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
-  ["/usr/local/bin/StratOS-configure-gnome"]="0:0:777"
-  ["/usr/local/bin/StratOS-configure-theme"]="0:0:777"
   ["/usr/local/bin/StratOS-configure-bedrock"]="0:0:777"
   ["/usr/local/bin/StratOS-configure-browser"]="0:0:777"
+  ["/usr/local/bin/StratOS-configure-gnome"]="0:0:777"
+  ["/usr/local/bin/StratOS-configure-theme"]="0:0:777"
   ["/usr/local/bin/StratOS-configure-ubuntu"]="0:0:777"
-  ["/usr/local/bin/install-bedrock-x86_64"]="0:0:777"
 )
